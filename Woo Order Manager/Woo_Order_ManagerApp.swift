7b0d88cfb00c9cedc04d18d0928ea699a7b3e355
Len Chan <lenchan139@tto.moe>
2025-09-08 22:46:21 +0800

    Initial Commit

---
+++ Woo Order Manager/Woo_Order_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Woo_Order_ManagerApp.swift
+//  Woo Order Manager
+//
+//  Created by Len Chan on 8/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct Woo_Order_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Woo Order ManagerTests/Woo_Order_ManagerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Woo_Order_ManagerTests.swift
+//  Woo Order ManagerTests
+//
+//  Created by Len Chan on 8/9/2025.
+//
+
+import Testing
+
+struct Woo_Order_ManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
